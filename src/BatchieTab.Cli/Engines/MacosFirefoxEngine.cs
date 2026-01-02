2bba44049e6c2bc80373349595cf0a7abe51fc2e
Idjaz Hossanee <idjazhossanee@gmail.com>
2026-01-03 00:28:27 +0400

    feat : macos firefox engine implementation + README update.

--- src/BatchieTab.Cli/Engines/MacosFirefoxEngine.cs
+++ src/BatchieTab.Cli/Engines/MacosFirefoxEngine.cs
@@ -1,14 +1,34 @@
+using System.Diagnostics;
+
 namespace BatchieTab.Cli.Engines;
 
 public class MacosFirefoxEngine : IEngine
 {
     public void Open(IEnumerable<string> urls)
     {
-        throw new NotImplementedException();
+        var args =
+            "-a \"Firefox\" " +
+            string.Join(" ", urls.Select(u => $"\"{u}\""));
+
+        StartProcess(args);
     }
 
     public void OpenIncognito(IEnumerable<string> urls)
     {
-        throw new NotImplementedException();
+        var args =
+            "-na \"Firefox\" --args -private-window " +
+            string.Join(" ", urls.Select(u => $"\"{u}\""));
+
+        StartProcess(args);
+    }
+
+    private static void StartProcess(string args)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "open",
+            Arguments = args,
+            UseShellExecute = false
+        });
     }
 }
