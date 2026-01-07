1feb0d5bffba31b9b11af03f97136f467af4bd4b
Idjaz Hossanee <idjazhossanee@gmail.com>
2026-01-07 23:29:08 +0400

    feat : add help page.

--- src/BatchieTab.Cli/Entry.cs
+++ src/BatchieTab.Cli/Entry.cs
@@ -8,10 +8,24 @@ namespace BatchieTab.Cli;
 public class Entry(IServiceProvider serviceProvider)
 {
     private const string BrowserArgumentName = "--browser";
+    private const string HelpArgumentName = "--help";
     private const string PathArgumentName = "--path";
     
     public void Run(string[] args)
     {
+        if (args.Contains(HelpArgumentName))
+        {
+            Console.WriteLine($"-- HOW TO --\n");
+            Console.WriteLine($"Complete usage with all possible arguments : <batchie-tab-script-file-name> --path <path-to-urls-file> --browser <{string.Join("|", EngineType.All)}> --incognito\n");
+            Console.WriteLine("-- ARGUMENTS --\n");
+            Console.WriteLine("--path\t\tArgument to support a custom urls file location. If not specified, BatchieTab will attempt to look for a file called 'batchie-tab-file' in the same location as the running script.");
+            Console.WriteLine($"--browser\tArgument to support choosing a specific browser. Values allowed are {string.Join(", ", EngineType.All)}.");
+            Console.WriteLine("--incognito\tArgument to support opening the list of tabs in an incognito window. If not specified, tabs will open in a normal / regular window.");
+            Console.WriteLine("--help\t\tDisplay this current help page. In case specified, all other arguments will not be considered and the help page will display while not opening any tab. Program would terminate after help page is displayed.");
+            
+            return;
+        }
+        
         string? browser = null;
         var path = "./batchie-tab-file";
         
